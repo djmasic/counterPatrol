dc6204e0e4537e1d76c7f5c9746038822c2799c0
Djordje Masic <dj.masic@levi9.com>
2023-03-29 11:57:30 +0200

    Added intentional test fail

--- integration_test/widget_test.dart
+++ integration_test/widget_test.dart
@@ -14,14 +14,12 @@ import 'package:counter/main.dart';
 void main() {
   patrolTest(
     'counter is incremented when plus button is tapped',
-    nativeAutomation: true,
     (PatrolTester $) async {
       await $.pumpWidget(const MyApp());
 
       expect($('0'), findsOneWidget);
-      expect($('-1'), findsOneWidget);
+      expect($('-1'), findsOneWidget); // itentionally failed expect
 
-      await Future.delayed(const Duration(seconds: 5));
       await $(Icons.add).tap();
 
       expect($('0'), findsNothing);
